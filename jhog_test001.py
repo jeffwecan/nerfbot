1e58e3545505383468a7203e0306655128778525
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2018-02-06 12:23:41 -0600

    test

--- jhog_test001.py
+++ jhog_test001.py
@@ -6,14 +6,14 @@ import time
 # ===========================================================================
 # Example Code for Adafruit 16 channel PWM Driver
 # Refactored by ruprecht
-# Numbers for HS-55 Servo (20ms/50Hz cycle, 900-2100 us width)
+# Numbers for HS-55 Servo (20ms/50Hz cycle, 1000-2000 us width)
 # ===========================================================================
-
+1
 # Initialise the PWM device using the default address 0x40
 pwm = PWM(0x40, debug=True)
 
-servoMin = 150  # Min pulse length, us (tick 184/4096)
-servoMax = 600  # Max pulse length, us  (tick 430/4096)
+servoMin = 1000  # Min pulse length, us (tick 184/4096)
+servoMax = 2000  # Max pulse length, us  (tick 430/4096)
 servoMid = servoMax - ((servoMax-servoMin)/2) # Midpoint pulse length, us
 cycle = 50 # cycle length, Hz
 
@@ -34,3 +34,28 @@ while (True):
   time.sleep(1)
   setServoPulse(0, servoMax)
   time.sleep(1)
+
+import RPi.GPIO as GPIO
+import time
+
+servo_pin = 5
+duty_cycle = 7.5     # Should be the centre for a SG90
+
+# Configure the Pi to use pin names (i.e. BCM) and allocate I/O
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin, GPIO.OUT)
+
+# Create PWM channel on the servo pin with a frequency of 50Hz
+pwm_servo = GPIO.PWM(servo_pin, 50)
+pwm_servo.start(duty_cycle)
+
+try:
+    while True:
+        duty_cycle = float(input("Enter Duty Cycle (Left = 5 to Right = 10):"))
+        pwm_servo.ChangeDutyCycle(duty_cycle)
+
+except KeyboardInterrupt:
+    print("CTRL-C: Terminating program.")
+finally:
+    print("Cleaning up GPIO...")
+    GPIO.cleanup()
