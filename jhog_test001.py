4911559c7a36344c3511598242743f0c6aab8aa3
Jeffrey Hogan <jeffrey.hogan@wpengine.com>
2018-02-06 12:25:10 -0600

    test

--- jhog_test001.py
+++ jhog_test001.py
@@ -12,8 +12,8 @@ import time
 # Initialise the PWM device using the default address 0x40
 pwm = PWM(0x40, debug=True)
 
-servoMin = 1100  # Min pulse length, us (tick 184/4096)
-servoMax = 1900  # Max pulse length, us  (tick 430/4096)
+servoMin = 1200  # Min pulse length, us (tick 184/4096)
+servoMax = 1800  # Max pulse length, us  (tick 430/4096)
 servoMid = servoMax - ((servoMax-servoMin)/2) # Midpoint pulse length, us
 cycle = 50 # cycle length, Hz
 
@@ -33,6 +33,8 @@ while (True):
   setServoPulse(0, servoMid)
   time.sleep(1)
   setServoPulse(0, servoMax)
+  time.sleep(1)
+  setServoPulse(0, servoMid)
   time.sleep(1)
 
 import RPi.GPIO as GPIO
